8f78dc2acdc5c7e3ed5e4b0f0890530a417a77c6
MickeiasDev <mickeiasdev@gmail.com>
2025-12-11 17:34:45 -0300

    Adicionar arquivos de projeto.

---
+++ AT_CSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AT_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> EXECUTANDO <<<");
+
+            Exercicio1.Run();
+
+            // Exercicio2.Run();
+            // Exercicio3.Run();
+            // Exercicio4.Run();
+            // Exercicio5.Run();
+
+            // Exercicio6.Run();
+
+            // Exercicio7.Run();
+            // Exercicio8.Run();
+
+            // Exercicio9.Run();
+            // Exercicio10.Run();
+            // Exercicio11e12.Run();
+
+            Console.WriteLine("\nExecução finalizada. Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
